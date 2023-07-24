ebd0b83c146be94f82467ba332a0c1dda2b70706
Astound Innovation Hub <provictor.ie@gmail.com>
2023-07-24 20:58:59 +0100

    clean-up old comps

--- src/views/PokemonListView.vue
+++ src/views/PokemonListView.vue
@@ -1,16 +1,12 @@
 <script setup>
 import NavBar from '../components/NavBar.vue'
-import PokemonCharacter from '../components/PokemonCharacter.vue'
 </script>
 
 <template>
   <main>
     <NavBar />
     <section class="max-w-screen-lg py-3 mx-auto">
-      <PokemonCharacter name="okay" />
-      <PokemonCharacter name="okay" />
-      <PokemonCharacter name="okay" />
-      <PokemonCharacter name="okay" />
+      <h1>Paginated list of pokemons</h1>
     </section>
   </main>
 </template>
